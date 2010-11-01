dffa755521ab9ace721cdbad4e894083855b0011
James Robey <james.robey@gmail.com>
2010-11-01 13:51:11 -0700

    incorporated design, fixed default value on input form behavior

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def text_field(object_name, method, options = {}) 
+    if options[:value]
+      options[:onfocus] = "if(this.value==this.defaultValue) this.value='';"
+      options[:onblur] = "if(this.value=='') this.value=this.defaultValue;"
+    end
+    super(object_name, method, options)
+  end
 end
