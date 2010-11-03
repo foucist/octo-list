dbb96268c3875b35f291c4ddcfcf5012581446c3
James Robey <james.robey@gmail.com>
2010-11-03 08:42:49 -0700

    Added acts_as_taggable

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,15 @@
 class Task < ActiveRecord::Base
+  acts_as_taggable
+
   named_scope :by_day, lambda { |d| { :conditions => {:created_at => d.beginning_of_day..d.end_of_day} } }
   named_scope :sticky, :conditions => {:sticky => true}
   named_scope :done, :conditions => {:done => true}
   named_scope :undone, :conditions => {:done => nil }
+
+  def after_create
+    self.tag_list = self.entry.scan(/#(\w+)/).join(', ')
+    self.save
+  end
 end
+
+
