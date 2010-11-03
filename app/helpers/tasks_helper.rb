d86f02a06cdce4ff1c01432015e5999a0a9e5ef4
James Robey <james.robey@gmail.com>
2010-11-03 10:15:40 -0700

    auto links tags and adds complete/sticky for css

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,14 @@
 module TasksHelper
+  def auto_link(x)
+    super(x).gsub(/#(\w*)/) { link_to("##{$1}", task_path($1), :id => "tag")  }
+  end
+
+  def type_of(x)
+    case
+    when x.done?
+      'id="complete"'
+    when x.sticky?
+      'id="sticky"'
+    end
+  end
 end
